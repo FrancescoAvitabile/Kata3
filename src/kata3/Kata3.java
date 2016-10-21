f5dacad4fabcebac18213e592bf3a0453c7db893
Franz Avitabile <F.Avitabile8@studenti.unisa.it>
2016-10-21 12:07:59 +0100

    Kata3 version 2. The Project implements a generical way to input data in the histogram

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -16,8 +16,22 @@ public class Kata3 {
 
     
     public static void main(String[] args) {
+         Histogram<String> histogram = new Histogram<>();
         
-        MostraGrafico mainGraf = new MostraGrafico("EMAIL CONTENUTE DALLE CASELLE DI POSTA");
+        histogram.insert("GMAIL.COM");
+        histogram.insert("HOTMAIL.COM");
+        histogram.insert("ALICEMAIL.COM");
+        histogram.insert("VIRGILIOMAIL.COM");
+        histogram.insert("ULPGC.ES");
+         histogram.insert("ALICEMAIL.COM");
+        histogram.insert("VIRGILIOMAIL.COM");
+        histogram.insert("ULPGC.ES");
+         histogram.insert("ALICEMAIL.COM");
+        histogram.insert("VIRGILIOMAIL.COM");
+        histogram.insert("ULPGC.ES");
+         histogram.insert("ALICEMAIL.COM");
+        histogram.insert("VIRGILIOMAIL.COM");
+        MostraGrafico mainGraf = new MostraGrafico("Grafico", histogram);
         
         
     }
